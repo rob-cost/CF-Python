e9761d0ffdcdca463b085c30387e509459258e7e
rob-cost <robertocostantino3@gmail.com>
2025-08-28 09:09:49 +0200

    Practiced with merging lists

--- exercise-1.5/shopping_list.py
+++ exercise-1.5/shopping_list.py
@@ -5,7 +5,7 @@ class ShoppingList(object):
         self.shopping_list = []
 
     def add_item(self, item):
-        if item not in self.shopping_list:
+        if not item in self.shopping_list:
             self.shopping_list.append(item)
 
     def remove_item(self, item):
@@ -14,9 +14,20 @@ class ShoppingList(object):
                 self.shopping_list.remove(item)
 
     def view_list(self):
-        print(f"({self.list_name} : {self.shopping_list}")
+        print("Items in " + str(self.list_name) + "\n" + 30*"-")
+        for item in self.shopping_list:
+            print(" - " + str(item))
+
+    def merge_list(self, obj):
+        merged_lists_name = "Merged list between: " + str(self.list_name) + " and " + str(obj.list_name)
+
+        merged_list_object = ShoppingList(merged_lists_name)
+
+        merged_list_object.shopping_list = self.shopping_list.copy()
+
 
 pet_store_list = ShoppingList("Pet Store Shopping List")
+
 
 pet_store_list.add_item("dog food")
 pet_store_list.add_item("frisbee")
